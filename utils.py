1e7f81bc62b2b8a89d7e7be6cc6bac75ca15270d
Olivia Foster Rhoades <ofoster@g.harvard.edu>
2021-10-07 20:18:35 -0400

    utils functions: thresh, rotate and plot

---
+++ utils.py
@@ -0,0 +1,130 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+import czifile
+from pathlib import Path
+import os
+from skimage import morphology, exposure, filters
+from skimage.feature import canny
+from skimage.measure import regionprops, regionprops_table, label
+from scipy import ndimage, misc
+
+def get_thresholded(data, method='OHTSU', shape_channel='DAPI', z_plane=None, bkgd=200):
+    if method.upper() == 'OHTSU':
+        ## THRESHOLD OHTSU
+        zshape = data[shape_channel].max(0)
+        bkgd = filters.threshold_otsu(zshape)
+        max_signal = np.max(zshape)-1
+        zshape = filters.apply_hysteresis_threshold(zshape, bkgd, max_signal)
+        zshape = filters.threshold_sauvola(zshape)
+        zshape = morphology.binary_closing(zshape)
+    elif method.upper() == 'GAMMA':
+        # GAMMA THRESHOLD
+        if z_plane is None:
+            z_plane = data[shape_channel].shape[0] // 2
+        zshape = data[shape_channel][z_plane,:,:]
+
+        max_signal = np.max(zshape)-1#np.quantile(zshape, 0.7)
+        zshape = exposure.adjust_gamma(zshape)
+        zshape = filters.apply_hysteresis_threshold(zshape, bkgd, max_signal)
+        zshape = morphology.binary_closing(zshape)
+    elif method.upper() == 'DILL':
+        # THRESHOLD DILATION
+        zshape = data[shape_channel].max(0)
+        zshape = exposure.adjust_gamma(zshape)
+        max_signal = np.max(zshape)-1
+        zshape = filters.apply_hysteresis_threshold(zshape, bkgd, max_signal)
+        footprint=morphology.disk(25) # size hard coded
+        zshape = morphology.binary_dilation(zshape, footprint)
+    elif method.upper() == 'GAMMA_V2':
+        # GAMMA THRESHOLD
+        if z_plane is None:
+            z_plane = data[shape_channel].shape[0] // 2
+        zshape = data[shape_channel][z_plane,:,:]
+        bkgd = filters.threshold_otsu(zshape)
+        max_signal = np.max(zshape)-1#np.quantile(zshape, 0.7)
+        zshape = exposure.adjust_gamma(zshape)
+        zshape = filters.apply_hysteresis_threshold(zshape, bkgd, max_signal)
+        zshape = morphology.binary_closing(zshape)
+
+    return zshape
+
+def get_orientation(data, method='OHTSU', shape_channel='DAPI', ap_channel='DAPI', z_plane=None, bkgd=200):
+    #get thresholded
+    zshape = get_thresholded(data, method, shape_channel, z_plane, bkgd)
+
+    max_AP = data[ap_channel].max(0) # this will be how to pick AP axis
+    
+    #get_dimensions
+    # emb_label= label(zshape.astype(np.uint8))
+    emb_regions = regionprops(zshape.astype(np.uint8))
+    areas = [emb['area'] for emb in emb_regions]
+    props = emb_regions[np.argmax(areas)]
+
+    y0, x0 = props.centroid
+    orientation = props.orientation
+    # long_axis = props.major_axis_length
+    # short_axis = props.minor_axis_length
+    # minr, minc, maxr, maxc = props.bbox
+    ys = [y0]
+    xs = [x0]
+    xs.append(x0 + math.cos(orientation) * 0.5 * props.minor_axis_length) #top
+    ys.append(y0 - math.sin(orientation) * 0.5 * props.minor_axis_length)
+    xs.append(x0 - math.sin(orientation) * 0.5 * props.major_axis_length)
+    ys.append(y0 - math.cos(orientation) * 0.5 * props.major_axis_length)
+    xs.append(x0 - math.cos(orientation) * 0.5 * props.minor_axis_length) #bottom
+    ys.append(y0 + math.sin(orientation) * 0.5 * props.minor_axis_length)            
+    xs.append(x0 + math.sin(orientation) * 0.5 * props.major_axis_length)
+    ys.append(y0 + math.cos(orientation) * 0.5 * props.major_axis_length)
+
+    rotation_axis = 90 - (orientation * 180/math.pi)
+    rotated_AP = ndimage.rotate(max_AP, rotation_axis)
+    rotated_AP = exposure.adjust_gamma(rotated_AP)
+    return zshape, rotation_axis, rotated_AP, xs, ys
+
+
+def show_orientation(zshape, rotated_AP, xs, ys, filename=None, path=None):
+    fig, ax = plt.subplots(1,2, figsize=(25,25))
+    ax[0].imshow(zshape, cmap=plt.cm.gray)
+    ax[0].plot((xs[3], xs[1]), (ys[3], ys[1]), '-y', linewidth=10) #minor axis line
+    ax[0].plot((xs[4], xs[2]), (ys[4], ys[2]), '-m', linewidth=10) #major axis line
+    ax[0].plot(xs[0], ys[0], '.g', markersize=25)
+
+    ax[1].imshow(rotated_AP, cmap=plt.cm.gray)
+
+        #time to save
+    if filename is not None:
+        if path is not None:
+            os.makedirs(path[:-4], exist_ok=True)
+            fig.savefig(os.path.join(path[:-4], f"{filename[:-4]}_APcheck"))
+        else:
+            fig.savefig(f"{filename[:-4]}_APcheck")
+    return fig
+
+
+def show_thresh_test(data, 
+                    methods_list=['OHTSU', 'GAMMA', 'DILL'], 
+                    shape_channel='DAPI', 
+                    ap_channel='DAPI', 
+                    z_plane=None, 
+                    bkgd=200):
+    fig, axs = plt.subplots(len(methods_list), 2)#, figsize=(10*len(methods_list), 20))
+    for i, method in enumerate(methods_list):
+        zshape, _, rotated_AP, xs, ys = get_orientation(data, method, shape_channel, ap_channel, z_plane, bkgd)
+        axs[i, 0].imshow(zshape, cmap=plt.cm.gray)
+        axs[i, 0].plot((xs[3], xs[1]), (ys[3], ys[1]), '-y', linewidth=10) #minor axis line
+        axs[i, 0].plot((xs[4], xs[2]), (ys[4], ys[2]), '-m', linewidth=10) #major axis line
+        axs[i, 0].plot(xs[0], ys[0], '.g', markersize=25)
+        axs[i, 0].set_title(f'{method}: masked')
+        axs[i, 1].imshow(rotated_AP, cmap=plt.cm.gray)
+        axs[i, 1].set_title(f'{method}: rotated')
+
+    return fig
+
+
+
+# def get_oriented_plane(data, z_plane, rotation_axis, flip_hor=False):
+#     output = {}
+#     for key, value in data.items():
+
