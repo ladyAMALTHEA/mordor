2653452442b806f96ae0ac39fd0c12a6376e7f2a
Olivia Foster Rhoades <ofoster@g.harvard.edu>
2021-10-10 17:07:23 -0400

    work on plotting

--- trace_utils.py
+++ trace_utils.py
@@ -1,6 +1,7 @@
 from imgproc_utils import *
 import csv
 from skimage.feature import canny
+import scipy.stats as stats
 
 def load_csv_database(filename):
     with open(filename, newline='') as csvfile:
@@ -11,22 +12,23 @@ def load_csv_database(filename):
     return database
 
 
-def get_all_traces(database, channel_dict, channel_list, work_dir, dv_channel, data_channel_list, z_offset=3, save=False):
+def get_all_traces(database, data_channel_list, channel_dict, channel_list, work_dir, dv_channel, z_offset=3, save=False):
     all_traces = {}
     for filename, info in database.items(): #for each file in the database
         this_dict = {}
         if not info['quality'].upper() == 'GOOD':
             continue
         this_dict['genotype'] = info['genotype']
-        this_dict['thresh_method'] = info['thresh_method']
-        this_dict['flip'] = info['flip'].upper() == 'TRUE'
+        thresh_method = info['thresh_method']
+        flip = info['flip'].upper() == 'TRUE'
         #load the data
         path = os.path.join(work_dir, filename)
         data, _ = load_data(path, channel_dict, channel_list)
         #start getting traces
-        this_dict['traces'], qc_imgs = get_traces(data, data_channel_list, this_dict['flip'], this_dict['thresh_method'], dv_channel, z_offset)
+        this_dict['traces'], qc_imgs = get_traces(data, data_channel_list, flip, thresh_method, dv_channel, z_offset)
+        make_qc_figs(qc_imgs, this_dict['traces'], save)
         ...
-        
+        #save figures
         
         all_traces[filename] = this_dict
     ...
@@ -48,31 +50,39 @@ def get_traces(data,
 
     #get orientation
     zshape, rotation_axis, rotated_AP, xs, ys = get_orientation(data, method, shape_channel, ap_channel, z_plane, bkgd)
-    dv_divide = make_mask(zshape. xs, ys)
+    dv_divide, im_filled = make_mask(zshape. xs, ys)
     rotated_zdv = orient(data[dv_channel][z_plane,:,:], rotation_axis, flip)
     dorsal_mask, dorsal, ventral = get_dv(dv_divide, rotated_zdv, bkgd)
 
+    qc_imgs = {}
+    qc_imgs['emb_mask'] = im_filled
+    qc_imgs['dv_divide'] = dv_divide
+    qc_imgs['dorsal_check'] = dorsal
+    qc_imgs['ventral_check'] = ventral
     traces = {}
     for channel in data_channel_list:
         channel_data = data[channel]
         these_traces = []
+        these_maskeds = []
         for z in z_planes:
             rotated = orient(channel_data[z,:,:], rotation_axis, flip)
             trace, masked = get_trace(rotated, dorsal_mask)
             these_traces.append(trace)
-            ...
-        #decide on figures
+            these_maskeds.append(masked)
+        traces[channel] = these_traces
+        qc_imgs[channel] = these_maskeds
 
     return traces, qc_imgs
 
-def get_trace(zplane, dorsal_mask):
+def get_trace(zplane, dorsal_mask, length=100):
     masked_zplane = zplane * dorsal_mask
     sum_trace = masked_zplane.sum(axis=0)
     mask_sum = dorsal_mask.sum(axis=0)
     mean_trace = sum_trace / mask_sum
     trace = mean_trace[mask_sum>0]
-    return trace, masked_zplane
-
+    inds = np.arange(trace.shape[0])
+    standard_trace, _, _ = stats.binned_statistic(inds, trace, bins=length)
+    return standard_trace, masked_zplane
 
 def make_mask(zshape, xs, ys):
     structure = np.ones((3, 3))
@@ -84,7 +94,7 @@ def make_mask(zshape, xs, ys):
     zorro = im_filled*edge_mask # slices off all the extra in edge_mask to make a mask of the area of interest in the embryo
     dv_divide = np.invert(knife) * zorro 
 
-    return dv_divide
+    return dv_divide, im_filled
 
 def make_knife(dims, xs, ys):
     major_axis_line = np.zeros(dims, dtype=bool)
@@ -141,7 +151,35 @@ def orient(zplane, rotation_axis, flip):
         rotated= np.fliplr(rotated)
     return rotated
 
-def format_trace_datastructure(trace_sets):
+def make_qc_figs(imgs, traces, save_name=None):
+    fig, axs = plt.subplots(2 + len(traces), 2, figsize=(10, 10 + 5*len(traces)))
+    axs[0,0].imshow(imgs['emb_mask'], title='Embryo Mask', cmap=plt.cm.gray)
+    axs[0,1].imshow(imgs['dv_divide'], title='Dorsal/Ventral Masks', cmap=plt.cm.gray)
+    axs[1,0].imshow(imgs['dorsal_check'], title='Dorsal Check', cmap=plt.cm.gray)
+    axs[1,1].imshow(imgs['ventral_check'], title='Ventral Check', cmap=plt.cm.gray)
+    for i, (channel, channel_traces) in enumerate(traces.items()):
+        mean = np.array(channel_traces).mean(0)
+        error = np.array(channel_traces).std(0)
+        axs[2+i, 0].fill_between(np.arange(mean.shape[0]), mean-error, mean+error, alpha=0.5, linewidth=0)
+        axs[2+i, 0].plot(mean)
+
+        axs[2+i, 1].imshow(imgs[channel][1], title=f'{channel} Check', cmap=plt.cm.gray)
+    if save_name is not None:
+        fig.savefig(save_name)
+
+
+def format_trace_datastructure(trace_sets, excluded=None):
     datastructure = {}
     for trace_set in trace_sets:
-        ...#organize genotypes
+        for filename, file_data in trace_set.items():
+            if excluded is not None and filename in excluded:
+                continue
+            genotype = file_data['genotype']
+            if genotype not in datastructure.keys():
+                datastructure[genotype] = {}
+            for gene, traces in file_data['traces'].items():
+                if gene not in datastructure[genotype].keys():
+                    datastructure[genotype][gene] = np.expand_dims(np.array(traces), 0) # [embryo, z_plane, trace] -> i.e. [# of embryos, 3, 100]
+                else:
+                    datastructure[genotype][gene] = np.append(datastructure[genotype][gene], np.array(traces), 0)
+    return datastructure
