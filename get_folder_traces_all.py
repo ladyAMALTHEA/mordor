5607dc85bb03281b75aeff2661fe52e19dbeeb0b
Olivia Foster Rhoades <ofoster@g.harvard.edu>
2021-10-11 12:40:41 -0400

    wrote plotting

--- get_folder_traces_all.py
+++ get_folder_traces_all.py
@@ -125,7 +125,7 @@ work_dir = '/n/scratch3/users/o/of12/images/psc'
 # Gt 488, Psc 546, Kr/Snail 647
 channel_dict = {'DAPI': 0, 'TL':1, 'gt':2, 'psc':3, 'kr':4}
 channel_list = ['DAPI', 'gt', 'psc', 'kr'] #keep for iteration
-data_channel_list = ['gt', 'kr', 'pho']
+data_channel_list = ['gt', 'kr', 'psc']
 shape_channel = 'DAPI'
 ap_channel = 'DAPI'
 dv_channel = 'kr'
@@ -154,7 +154,7 @@ work_dir = '/n/scratch3/users/o/of12/images/sfmbt'
 # Gt 488, Sfmbt 546, Kr/Snail 647
 channel_dict = {'DAPI': 0, 'TL':1, 'gt':2, 'sfmbt':3, 'kr':4}
 channel_list = ['DAPI', 'gt', 'sfmbt', 'kr'] #keep for iteration
-data_channel_list = ['gt', 'kr', 'pho']
+data_channel_list = ['gt', 'kr', 'sfmbt']
 shape_channel = 'DAPI'
 ap_channel = 'DAPI'
 dv_channel = 'kr'
@@ -182,7 +182,7 @@ work_dir = '/n/scratch3/users/o/of12/images/ez'
 # Gt 488, Ez 546, Kr/Snail 647
 channel_dict = {'DAPI': 0, 'TL':1, 'gt':2, 'ez':3, 'kr':4}
 channel_list = ['DAPI', 'gt', 'ez', 'kr'] #keep for iteration
-data_channel_list = ['gt', 'kr', 'pho']
+data_channel_list = ['gt', 'kr', 'ez']
 shape_channel = 'DAPI'
 ap_channel = 'DAPI'
 dv_channel = 'kr'
