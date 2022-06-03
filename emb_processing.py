d4174be19ad8d28d29450fdedfd6e22e669e4188
Olivia Foster Rhoades <ofoster@g.harvard.edu>
2022-06-03 15:54:03 -0400

    final plots

--- emb_processing.py
+++ emb_processing.py
@@ -6,9 +6,31 @@ from trace_utils import *
 
 
 if __name__ == '__main__':
-    #WT CAD data
-    csv_file = 'settings_CAD_wt.csv'
-    work_dir = '/n/groups/depace/of12/images/CAD_gt/wt_CAD'
+   #WBCD and hb data
+    csv_file = 'settings_BCDhb_final.csv'
+    work_dir = '/n/groups/depace/of12/images/BCD_hb'
+    # BCD 1:100 555, hb/Snail 647
+    channel_dict = {'DAPI': 0, 'TL':1, 'BCD':2, 'hb':3}
+    channel_list = ['DAPI', 'BCD', 'hb'] #keep for iteration
+    data_channel_list = ['BCD', 'hb']
+    shape_channel = 'DAPI'
+    ap_channel = 'DAPI'
+    dv_channel = 'hb'
+    z_offset = 3
+
+    database = load_csv_database(os.path.join(work_dir, csv_file))
+    all_traces = get_all_traces(database, 
+                                data_channel_list, 
+                                channel_dict, 
+                                channel_list, 
+                                work_dir, 
+                                dv_channel, 
+                                z_offset, 
+                                save=True)
+                                
+    #WT CAD data without Giant
+    csv_file = 'settings_CAD_wt_nogt_final.csv'
+    work_dir = '/n/groups/depace/of12/images/CAD_gt/wt_CAD_nogt'
     # Pho 488, CAD 555, giant/Snail 647
     channel_dict = {'DAPI': 0, 'TL':1, 'CAD':3, 'pho':2, 'gt':4}
     channel_list = ['DAPI', 'CAD', 'pho', 'gt'] #keep for iteration
@@ -29,12 +51,12 @@ if __name__ == '__main__':
                                 save=True)
     
     #Pho CAD data
-    csv_file = 'settings_CAD_pho.csv'
-    work_dir = '/n/groups/depace/of12/images/CAD_gt/pho_CAD'
+    csv_file = 'settings_CADgt_final.csv'
+    work_dir = '/n/groups/depace/of12/images/CAD_gt/CADgt'
     # Pho 488, CAD 555, giant/Snail 647
     channel_dict = {'DAPI': 0, 'TL':1, 'CAD':3, 'pho':2, 'gt':4}
     channel_list = ['DAPI', 'CAD', 'pho', 'gt'] #keep for iteration
-    data_channel_list = ['CAD', 'pho']
+    data_channel_list = ['CAD', 'pho', 'gt']
     shape_channel = 'DAPI'
     ap_channel = 'DAPI'
     dv_channel = 'gt'
@@ -51,7 +73,7 @@ if __name__ == '__main__':
                                 save=True)
     
     # Extra Sex combs gap gene data
-    csv_file = 'settings_esc_gaps.csv'
+    csv_file = 'settings_esc_gaps_final.csv'
     work_dir = '/n/groups/depace/of12/images/esc_gaps'
     # Gt 488, Kr/Snail 546, Hb 647
     channel_dict = {'DAPI': 0, 'TL':1, 'gt':2, 'kr':3, 'hb':4}
@@ -74,7 +96,7 @@ if __name__ == '__main__':
    
 
     # Giant and Hunchback Data
-    csv_file = 'settings_gthb.csv'
+    csv_file = 'settings_gthb_final.csv'
     work_dir = '/n/groups/depace/of12/images/gt_hb'
     # Gt 488, Pho 546, Snail/Hb 647
     channel_dict = {'DAPI': 0, 'TL':1, 'gt':2, 'pho':3, 'hb':4}
@@ -96,27 +118,27 @@ if __name__ == '__main__':
                                 save=True)
 
     
-    # #BCD and Kr data for Pho and WT
-    # csv_file = 'settings_BCDKr.csv'
-    # work_dir = '/n/groups/depace/of12/images/BCD_kr'
-    # # Snail/Kr 488, BCD 555, Pho 647
-    # channel_dict = {'DAPI': 0, 'TL':1, 'kr':2, 'BCD':3, 'pho':4}
-    # channel_list = ['DAPI', 'kr', 'BCD', 'pho'] #keep for iteration
-    # data_channel_list = ['kr', 'BCD', 'pho']
-    # shape_channel = 'DAPI'
-    # ap_channel = 'DAPI'
-    # dv_channel = 'kr'
-    # z_offset = 3
+    #BCD and Kr data for Pho and WT
+    csv_file = 'settings_BCDKr_final.csv'
+    work_dir = '/n/groups/depace/of12/images/BCD_kr'
+    # Snail/Kr 488, BCD 555, Pho 647
+    channel_dict = {'DAPI': 0, 'TL':1, 'pho':2, 'BCD':3, 'kr':4}
+    channel_list = ['DAPI', 'kr', 'BCD', 'pho'] #keep for iteration
+    data_channel_list = ['kr', 'BCD', 'pho']
+    shape_channel = 'DAPI'
+    ap_channel = 'DAPI'
+    dv_channel = 'kr'
+    z_offset = 3
 
-    # database = load_csv_database(os.path.join(work_dir, csv_file))
-    # all_traces = get_all_traces(database, 
-    #                             data_channel_list, 
-    #                             channel_dict, 
-    #                             channel_list, 
-    #                             work_dir, 
-    #                             dv_channel, 
-    #                             z_offset, 
-    #                             save=True)
+    database = load_csv_database(os.path.join(work_dir, csv_file))
+    all_traces = get_all_traces(database, 
+                                data_channel_list, 
+                                channel_dict, 
+                                channel_list, 
+                                work_dir, 
+                                dv_channel, 
+                                z_offset, 
+                                save=True)
 
     # #knirps and eve
     # csv_file = 'settings_knieve.csv'
